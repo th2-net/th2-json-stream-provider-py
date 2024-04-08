4c5855ae6317de137b2126c8f4b6fd437cd12edf
molotgor <molotgog@gmail.com>
2024-04-08 11:19:13 +0400

    initial server for running jupyter with parameters

---
+++ jupyterServ.py
@@ -0,0 +1,133 @@
+import papermill as pm
+from aiohttp.web_request import Request
+from aiohttp import web
+from aiojobs.aiohttp import setup, spawn
+from aiohttp_swagger import *
+import json
+import os.path
+import asyncio
+
+serverStatus = 'idle'
+
+async def reqStatus(req: Request):
+    """
+    ---
+    description: This end-point allow to test that service is up.
+    tags:
+    - Health check
+    produces:
+    - application/json
+    responses:
+        "200":
+            description: successful operation. Return json with server status
+    """
+    return web.json_response({'status': serverStatus})
+
+async def reqFiles(req: Request):
+    """
+    ---
+    description: This end-point allows to get files that could be requested.
+    tags:
+    - File operation
+    produces:
+    - application/json
+    responses:
+        "200":
+            description: successful operation. Return string array of available files.
+    """
+    from glob import glob
+    files = glob('*.ipynb') + glob('*.json')
+    return web.json_response(files)
+
+async def reqArguments(req: Request):
+    """
+    ---
+    description: This end-point allows to get parameters for file in requested path.
+    tags:
+    - File operation
+    produces:
+    - application/json
+    responses:
+        "200":
+            description: successful operation. Return json of file's parameters.
+        "404":
+            description: requested file doesn't exist.
+    """
+    path = req.rel_url.query['path']
+    if not path or not os.path.isfile(path):
+        return web.HTTPNotFound()
+    params = pm.inspect_notebook(path)
+    return web.json_response(params)
+
+async def launchNotebook(input, arguments = None):
+        pm.execute_notebook(input, 'lauchLogs.json', arguments)
+
+async def reqLaunch(req: Request):
+    """
+    ---
+    description: This end-point allows to get file's parameters for requested path.
+    tags:
+    - Execution operation
+    produces:
+    - application/json
+    responses:
+        "200":
+            description: successful operation. Return json with path for resulting file.
+        "400":
+            description: body with parameters not present.
+        "404":
+            description: requested file doesn't exist.
+        "503":
+            description: server is currently busy.
+    """
+    if serverStatus != 'idle':
+        return web.HTTPServiceUnavailable(reason='server is currently busy')
+    if not req.can_read_body:
+        return web.HTTPBadRequest(reason='body with parameters not present')
+    path = req.rel_url.query['path']
+    if not path or not os.path.isfile(path):
+        return web.HTTPNotFound()
+    from uuid import uuid4
+    output_path = '%s.json' % str(uuid4())
+    parameters = await req.json()
+    parameters['output_path'] = output_path
+    asyncio.shield(spawn(req, launchNotebook(path, parameters)))
+    return web.json_response({'path': output_path})
+        
+async def reqResult(req: Request):
+    """
+    ---
+    description: This end-point allows to get result from requested path.
+    tags:
+    - Execution operation
+    produces:
+    - application/json
+    responses:
+        "200":
+            description: successful operation. Return resulting file's json.
+        "400":
+            description: body with parameters not present.
+        "404":
+            description: requested file doesn't exist.
+        "503":
+            description: server is currently busy.
+    """
+    if serverStatus != 'idle':
+        return web.HTTPServiceUnavailable(reason='server is currently busy')
+    path = req.rel_url.query['path']
+    if not path or not os.path.isfile(path):
+        return web.HTTPNotFound()
+    file = open(path, "r")
+    result = json.load(file)
+    return web.json_response(result)
+
+if __name__ == '__main__':
+    app = web.Application()
+    setup(app)
+    app.router.add_route('GET', "/status", reqStatus)
+    app.router.add_route('GET', "/files/all", reqFiles)
+    app.router.add_route('GET', "/files", reqArguments)
+    app.router.add_route('POST', "/execute", reqLaunch)
+    app.router.add_route('GET', "/result", reqResult)
+    setup_swagger(app)
+    web.run_app(app)
